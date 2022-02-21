da95f467f805ecad28bfe7475b2f966fffdda42c
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-02-21 19:22:08 +0200

    activatedEmitter was changed to  Subject

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,7 +1,11 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 
 
 @Injectable({providedIn: 'root'})
 export class UserService {
-    activatedEmitter = new EventEmitter<boolean>();
+    // activatedEmitter = new EventEmitter<boolean>();
+
+    activatedEmitter = new Subject<boolean>();
+    //activatedEmitter was changed to  Subject - generic type
 }
